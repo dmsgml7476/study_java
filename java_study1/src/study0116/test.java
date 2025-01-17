72859c0ba56ea3f179a37e89e8291151efa50d88
dmsgml7476 <dmsgml7476@gmail.com>
2025-01-17 12:49:24 +0900

    변수, 데이터타입, 형변환

--- java_study1/src/study0116/test.java
+++ java_study1/src/study0116/test.java
@@ -24,6 +24,96 @@ public class test {
 		age = 27;
 		age = '가'; // 자바에서 '' 표현은 문자를 의미
 				   // "" 표현은 문자열을 의미
+	    System.out.println(27);
+	    System.out.println(27);
+	    
+	    byte num1 = 120;
+	    System.out.println(num1);
+//	    num1 = 130    // 오류 : byte(127) 범위 초과
+	    
+	    int num2 = 20; // 변수 선언하면서 데이터 저장 - 변수 초기화
+	    
+	    int num3 = 43; 
+//	    num3은 43이다. 라고 하면 안됨. num3과 43이 같다는 표현이라.
+//	    num3에 43을 저장했다, num3에 43을 대입했다. 라고 표현
+	    
+	    System.out.println();
+	   	System.out.println(num2 + num3);
+	   	
+	   	int sum = num2 + num3;
+	   	System.out.println(sum);
+	   	
+//	   	사각형의 너비와 높이 값으로 넓이를 구하세요.
+//	   	
+//	   	int area = width + height;
+//	   	int width=20;
+//	   	int height=30;
+//	   	
+	   	// 코드 실행은 순차적이기 때문에 area 변수 값이 존재하지 않는 변수이기때문에 오류이다.
+//	   	
+//	   	int width;
+//	   	int height;
+//	   	int area = width + height;
+//	   	int width=20;
+//	   	int height=30;
+	   	
+	   	// 변수는 선언했지만, 변수의 값이 지정되지 않았기때문에 area에서 변수 이름으로 이용할 수 없다.
+	   	
+	   	int blue = 20;
+	   	int red = 15;
+	   	int white = 0;
+	   	
+	   	// 파란컵에 오랜지주스가 20ml있다.
+	   	// 빨간컵에 딸기 주스가 15ml 있다.
+	   	// 파란컵에 딸기주스를, 빨간컵에 오랜지주스를 담으려면 어떻게 해야하는가?
+	   	// blue와 red 값을 출력
+	   	// blue=15, red=20 은 아님
+	   	
+	   	white = blue;
+	   	blue = red;
+	   	red = white;
+	   	
+	   	System.out.println(blue);
+	   	
+	   	System.out.println(red);
+	   	
+	   	int birthYear = 1991;
+	   	// 김철수가 태어난 년도를 birth_year에 저장했다.
+	   	// 나이는??
+	   
+	   	int myAge = 2025 - birthYear;
+	   	
+	    System.out.println(myAge);
+	    
+	    // 변수란 데이터(값)을 저장할 수 있는 메모리 공간
+	    // 변수의 공간에는 데이터타입에 맞는 데이터만 저장 할 수 있게 해줘야한다.
+	    // 변수의 선언은 데이터 타입 변수명 (예 - int num;)
+	    // 변수이름은 카멜 표기법이 일반적이다.
+	    // 변수이름은 변수의 사용목적에 맞는 이름으로 한다.(길어도됨)
+	    
+	    float fnum = 2.156f;  // 자바에서는 실수를 표현할때 뒤에 f 를 붙여준다!
+	    System.out.println(fnum)
+	    ;
+	    fnum = 2.123456789f;
+	    System.out.println(fnum);
+	    double dnum = 1.1234567890123456789;
+	    System.out.println(dnum);
+	    
+	    char ch='a';
+	    System.out.println(ch);
+	    ch = '가';   // char 는 문자열을 처리 할 수 없다. "" <= 문자열로 처리되기 때문에 반드시 '' <= 문자로 인식되는 작은 따옴표를 사용한다.
+	    System.out.println(ch);
+	    ch = 97;
+	    System.out.println(ch);
+	    ch = 'H';
+	    System.out.println(ch - 'A');
+	    System.out.println('J' - 64);
+	    
+	    String name = "홍길동";  // 자바에서 문자열 사용하기 위해선 String이라는 클래스 타입 사용
+	    
+	    boolean isApple = true; // 참, 거짓 값을 가지는 타입.
+	    
+	    
 	}
-
+	
 }
---
+++ java_study1/src/study0117/Test2.java
@@ -0,0 +1,15 @@
+package study0117;
+
+public class Test2 {
+	public static void main(String[] args) {
+		
+		int num = 20;
+		float pi = 3.14f;
+		
+		num = (int) pi;   //  강제 형변환 =일시적으로 타입을 바꿈.
+		pi = num;     // 자동 형변환
+		
+		//데이터 타입 바꾸는 방식 2가지 : 강제 형변환 (큰쪽에서 작은쪽으로 들어갈때) | 자동 형변환 (작은쪽에서 큰 쪽으로 들어갈때)
+	}
+
+}
