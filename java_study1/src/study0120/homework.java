54f5864191e416a1ec85fe46437f67e9df3528cb
dmsgml7476 <dmsgml7476@gmail.com>
2025-01-20 15:34:44 +0900

    dds

---
+++ java_study1/src/study0120/homework.java
@@ -0,0 +1,15 @@
+package study0120;
+
+public class homework {\
+	int appleBox = 20;
+	int orangeBox = 45;
+	int appleBoxnum = 11;
+	int orangeBoxnum = 12;
+
+	int totalApple = appleBox * appleBoxnum;
+	int totalOrange = orangeBox * orangeBoxnum;
+
+	int totalNum = totalApple + totalOrange;
+
+	System.out.println("과일 총 개수 : " + totalNum);
+}
