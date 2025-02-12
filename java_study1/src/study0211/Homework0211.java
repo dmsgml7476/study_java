f5778f0f49762ab81bc5f58dd001f726d0917c9a
dmsgml7476 <dmsgml7476@gmail.com>
2025-02-12 09:12:34 +0900

    0212 빙고

--- java_study1/src/study0211/Homework0211.java
+++ java_study1/src/study0211/Homework0211.java
@@ -7,29 +7,32 @@ public class Homework0211 {
 
 	public static void main(String[] args) {
 		
+
 		Scanner scan = new Scanner(System.in);
+
 		
 		int[] game = new int[25];
 		
 		int idx=0;
-		
 		// game 배열에 값 채우기
-
-		int temp = (int) (Math.random()*50+1);
-		boolean isSame = false;
-		for (int i = 0; i<idx; i++) {
-			if(game[i] == temp)
-				isSame=true;
+		
+		while(true) {
+			int temp = (int) (Math.random()*25+1);
+			boolean isSame = false;
+			for (int i=0; i<idx; i++) {
+				if (game[i]==temp)
+					isSame=true;
+			}
+			if (!isSame)
+				game[idx++]= temp;
+			if (idx==game.length) break;
 		}
-		if (!isSame)
-			game[idx++] = temp;
-//		System.out.println(Arrays.toString(game));
 		
 
 		
-		
-		// 5줄 5칸 출력
-	
+//		
+//		// 5줄 5칸 출력
+//	
 		while(true) {
 			for (int i=0; i<=4; i++) { // 빙고판 출력부분
 				for (int k=0; k<=4; k++) {
@@ -54,14 +57,18 @@ public class Homework0211 {
 				}
 			}
 		}
+//		
+//		// 빙고게임은 5줄 빙고 되면 종료되도록.
+//		// 1줄 빙고인지, 두줄 빙고인지 확인할 수 있는 코드 필요. (인덱스의 규칙을 잘 보아라. i와 k를 잘보아라.)
+//		
+//		// 숫자 중복으로 안들어가도록 하기.
+//		
+//		
+//		// (추가) 컴퓨터도 하도록 2인용 게임으로 만들기(컴퓨터도 게임 승리를 위한 선택을 하도록)
+//		
+//		
+
 		
-		// 빙고게임은 5줄 빙고 되면 종료되도록.
-		// 1줄 빙고인지, 두줄 빙고인지 확인할 수 있는 코드 필요. (인덱스의 규칙을 잘 보아라. i와 k를 잘보아라.)
-		
-		// 숫자 중복으로 안들어가도록 하기.
-		
-		
-		// (추가) 컴퓨터도 하도록 2인용 게임으로 만들기(컴퓨터도 게임 승리를 위한 선택을 하도록)
 		
 		
 		
