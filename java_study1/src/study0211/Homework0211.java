a5c9d192d34398ede20a6fee177e827ce31effa7
dmsgml7476 <dmsgml7476@gmail.com>
2025-02-13 18:00:58 +0900

    빙고!

--- java_study1/src/study0211/Homework0211.java
+++ java_study1/src/study0211/Homework0211.java
@@ -50,58 +50,57 @@ public class Homework0211 {
 				}
 				System.out.println();
 			}
-		
+		    
+			
+			if (bingo == 5) {		    	
+				System.out.println("빙고 승리!");
+				break;}
+			
+			
 		    
 			System.out.print("숫자 선택 : "
 					+ "");
 			int num = scan.nextInt(); // 빙고판에 있는 숫자 입력
-			
-			if (bingo == 5) {		    	
-				System.out.println("빙고!");
-				break;}
-				
+					
 			
 			for (int i=0; i<game.length; i++) {
 				if (game[i] == num) {// 내가 입력한 숫자를 배열에서 찾기
 					game[i] = 0;
-					if (i<=4 || i==0) wit_1++;
-					else if (i<=9) wit_2++;
-					else if (i<=14) wit_3++;
-					else if (i<=19) wit_4++;
-					else if (i<=24) wit_5++;
+					if (i<=4 || i==0) {wit_1++;}
+					else if (i<=9) {wit_2++;}
+					else if (i<=14) {wit_3++;}
+					else if (i<=19) {wit_4++;}
+					else if (i<=24) {wit_5++;}
 					
 					
-					if (i%5 == 0) len_1++;
-					if (i%5 == 1) len_2++;
-					if (i%5 == 2) len_3++;
-					if (i%5 == 3) len_4++;
-					if (i%5 == 4) len_5++;
+					if (i%5 == 0) {len_1++;}
+					if (i%5 == 1) {len_2++;}
+					if (i%5 == 2) {len_3++;}
+					if (i%5 == 3) {len_4++;}
+					if (i%5 == 4) {len_5++;}
 					
-					if (i%6 == 0) cro_1++;
-					if (i%4 == 0  && i != 0 && i != 24) cro_2++;
+					if (i%6 == 0) {cro_1++;}
+					if (i%4 == 0  && i != 0 && i != 24) {cro_2++;}
 				}
 				
 			}
-			if (wit_1 == 5) bingo++; wit_1 =0;
-			if (wit_2 == 5) bingo++; wit_2 =0;
-			if (wit_3 == 5) bingo++; wit_3 =0;
-			if (wit_4 == 5) bingo++; wit_4 =0;
-			if (wit_5 == 5) bingo++; wit_5 =0;
+			if (wit_1 == 5) {bingo++; System.out.println(bingo + "줄 빙고 성공!"); wit_1 =0;}
+			if (wit_2 == 5) {bingo++; System.out.println(bingo + "줄 빙고 성공!"); wit_2 =0;}
+			if (wit_3 == 5) {bingo++; System.out.println(bingo + "줄 빙고 성공!"); wit_3 =0;}
+			if (wit_4 == 5) {bingo++; System.out.println(bingo + "줄 빙고 성공!"); wit_4 =0;}
+			if (wit_5 == 5) {bingo++; System.out.println(bingo + "줄 빙고 성공!"); wit_5 =0;}
 			
-			if (len_1 == 5) bingo++; len_1 =0;
-			if (len_2 == 5) bingo++; len_2 =0;
-			if (len_3 == 5) bingo++; len_3 =0;
-			if (len_4 == 5) bingo++; len_4 =0;
-			if (len_5 == 5) bingo++; len_5 =0;
+			if (len_1 == 5) {bingo++; System.out.println(bingo + "줄 빙고 성공!"); len_1 =0;}
+			if (len_2 == 5) {bingo++; System.out.println(bingo + "줄 빙고 성공!"); len_2 =0;}
+			if (len_3 == 5) {bingo++; System.out.println(bingo + "줄 빙고 성공!"); len_3 =0;}
+			if (len_4 == 5) {bingo++; System.out.println(bingo + "줄 빙고 성공!"); len_4 =0;}
+			if (len_5 == 5) {bingo++; System.out.println(bingo + "줄 빙고 성공!"); len_5 =0;}
 			
-			if (cro_1 == 5) bingo++; cro_1 =0;
-			if (cro_2 == 5) bingo++; cro_2 =0;
+			if (cro_1 == 5) {bingo++; System.out.println(bingo + "줄 빙고 성공!"); cro_1 =0;}
+			if (cro_2 == 5) {bingo++; System.out.println(bingo + "줄 빙고 성공!"); cro_2 =0;}
 			
 			System.out.println(bingo);
-			if (bingo == 5) {
-				System.out.println("빙고!");
-				break;
-			}
+			
 			
 		}
 		
